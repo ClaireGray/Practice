641f99003795273d48193d46d8477805bd0c478a
sungyuri23 <claire.ancajas@ttu.edu>
2015-10-05 22:58:03 -0500

    insertion code in java

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        int[] arrayList = {5, 2, 4, 6, 7};
+        insertionSort(arrayList);
+        printArray(arrayList);
+
+    }
+    public static int[] insertionSort(int[] A) {
+        for (int j = 1; j < A.length; j++) {
+            int key = A[j];
+            int i = j - 1;
+            while (i >= 0 && A[i] > key) {
+                A[i + 1] = A[i];
+                i--;
+            }
+            A[i + 1] = key;
+        }
+        return A;
+    }
+    public static void printArray(int [] A){
+        for (int i = 0; i < A.length; i++){
+            System.out.print(" " + A[i]);
+
+        }
+    }
+}
